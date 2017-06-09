1dc79bdf392855d30f5294658c4e1380b8e7975b
kia-h <mhey237@gmail.com>
2017-06-09 22:49:40 +1000

    Add Script bundle

--- BookService/App_Start/BundleConfig.cs
+++ BookService/App_Start/BundleConfig.cs
@@ -20,6 +20,11 @@ namespace BookService
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            // New code:
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                      "~/Scripts/knockout-{version}.js",
+                      "~/Scripts/app.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
