29a477adab3429370f9ee767b907752d60736155
kia-h <mhey237@gmail.com>
2017-06-10 18:38:52 +1000

    Fix some bugs in app.js regarding add new book function

--- BookService/Scripts/app.js
+++ BookService/Scripts/app.js
@@ -2,8 +2,18 @@
     var self = this;
     self.books = ko.observableArray();
     self.error = ko.observable();
+    self.detail = ko.observable();
+    self.authors = ko.observableArray();
+    self.newBook = {
+        Author: ko.observable(),
+        Genre: ko.observable(),
+        Price: ko.observable(),
+        Title: ko.observable(),
+        Year: ko.observable()
+    }
 
     var booksUri = '/api/books/';
+    var authorsUri = '/api/authors/';
 
     function ajaxHelper(uri, method, data) {
         self.error(''); // Clear error message
@@ -18,7 +28,11 @@
         });
     }
 
-    self.detail = ko.observable();
+    function getAllBooks() {
+        ajaxHelper(booksUri, 'GET').done(function (data) {
+            self.books(data);
+        });
+    }
 
     self.getBookDetail = function (item) {
         ajaxHelper(booksUri + item.Id, 'GET').done(function (data) {
@@ -26,48 +40,30 @@
         });
     }
 
+    function getAuthors() {
+        ajaxHelper(authorsUri, 'GET').done(function (data) {
+            self.authors(data);
+        });
+    }
 
-    function getAllBooks() {
-        ajaxHelper(booksUri, 'GET').done(function (data) {
-            self.books(data);
+
+    self.addBook = function (formElement) {
+        var book = {
+            AuthorId: self.newBook.Author().Id,
+            Genre: self.newBook.Genre(),
+            Price: self.newBook.Price(),
+            Title: self.newBook.Title(),
+            Year: self.newBook.Year()
+        };
+
+        ajaxHelper(booksUri, 'POST', book).done(function (item) {
+            self.books.push(item);
         });
     }
 
     // Fetch the initial data.
     getAllBooks();
+    getAuthors();
 };
-
-self.authors = ko.observableArray();
-self.newBook = {
-    Author: ko.observable(),
-    Genre: ko.observable(),
-    Price: ko.observable(),
-    Title: ko.observable(),
-    Year: ko.observable()
-}
-
-var authorsUri = '/api/authors/';
-
-function getAuthors() {
-    ajaxHelper(authorsUri, 'GET').done(function (data) {
-        self.authors(data);
-    });
-}
-
-self.addBook = function (formElement) {
-    var book = {
-        AuthorId: self.newBook.Author().Id,
-        Genre: self.newBook.Genre(),
-        Price: self.newBook.Price(),
-        Title: self.newBook.Title(),
-        Year: self.newBook.Year()
-    };
-
-    ajaxHelper(booksUri, 'POST', book).done(function (item) {
-        self.books.push(item);
-    });
-}
-
-getAuthors();
 
 ko.applyBindings(new ViewModel());
